1de999bb0d1baa9cba6108ea820c84a4df4d8fb3
Yochi <993520026@qq.com>
2024-03-29 16:05:49 +0800

    windows version diff

--- listening/ErrorBookGenerate.py
+++ listening/ErrorBookGenerate.py
@@ -1,16 +1,19 @@
 import datetime
 import os
+from pprint import pprint
 
 #get file from last commit
 # Files = os.("git diff --name-only HEAD HEAD^").read().split("\n")
-Files = os.popen("git diff --name-only HEAD HEAD^").read().split("\n")
-print("PWD: ",os.getcwd())
-print("Files: ", Files,os.popen("git diff --name-only HEAD HEAD^").read())
+Files = os.popen("git diff --name-only HEAD HEAD^^^^").read().split("\n")
+    # 在windows中, ^是转义符, 要想敲一个^, 需要敲^^
+# pprint(Files)
 #get time as MONTH-DAY
-time = datetime.datetime.now().strftime("%m%d%h")
+time = datetime.datetime.now().strftime("%m%d%H")
 for file in Files:
+    # print("DEBUG: ", file, file.startswith("listening/test"), os.path.exists(file))
     if(os.path.exists(file) and file.startswith("listening/test")):
         filename = os.path.basename(file)
         print(filename)
-        os.system(f"diff --ignore-space-change -y --suppress-common-lines listening/ref/{filename} listening/test/{filename} > listening/err/{filename}.{time}.txt")
+        os.system(f"@fc /w /c /l /n listening\\ref\\{filename} listening\\test\\{filename} > listening\\err\\{filename}.{time}.txt")
+        # os.system(f"fc /w /c /l /n listening/ref/{filename} listening/test/{filename} > listening/err/{filename}.{time}.txt")
 # os.system(f"git add err && git commit -m \"update {time}'s error books\" && git push")
